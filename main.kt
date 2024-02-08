3c0c281102c7be4c50a14d1ec6ce5cd94f648982
aine-mbabazi <patainembabazi@gmail.com>
2024-02-08 22:02:32 +0300

    created a new file

---
+++ main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var fullName = "Ainembabazi Patricia"
+    var  age = 20
+    var  phoneNumber = "+256 773698766"
+    var weight: Float = 70.68F
+    var citizenOfKenya: Boolean = true
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    print(citizenOfKenya)
